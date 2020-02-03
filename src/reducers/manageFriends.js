68f0a24a03051540327994d66b97e304f7833e18
DBuckley0126 <DannyBoy.0126>
2020-02-03 18:42:11 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
-export function manageFriends(state, action){
+// export function manageFriends(state = {friends: []}, action){
+
+//   switch (action.type){
+//     case 'ADD_FREIND':
+//       return {...state,friends: [...state.friends, action.friend]}
+//     case 'REMOVE_FREIND':
+//       return {...state, friends: state.friends.filter(friend =>friend.id === action.id)}  
+//     default: 
+//       return state  
+//   } 
+// }
+
+export function manageFriends(state = {friends:[]}, action) {
+
+  switch(action.type){
+    case "ADD_FRIEND":
+      return {...state,friends: [...state.friends, action.friend]}
+    case "REMOVE_FRIEND":
+
+      return {...state, friends: state.friends.filter(friend => {return friend.id !== action.id})}
+
+    default:
+        return state
+  }
 }
