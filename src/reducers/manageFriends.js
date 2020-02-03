9c02d5867eec8fd99a24a1ebcdcc11cfc6fd04d9
Doug Kintop <dkintop@hotmail.com>
2020-02-03 13:26:36 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,15 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  
+  
+    swtich(action.type){
+        
+        case 'ADD_FRIEND':
+
+        return {friends: state.friends.push(action.friend)}
+
+
+        case 'REMOVE_FRIEND':
+
+        default: return state
+  };
 }
