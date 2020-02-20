331b3e5beadb119df035da990f0063f777529009
dean <deanlloyd64@gmail.com>
2020-02-20 00:59:15 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: []}, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return {...state, friends: [state.friends[0], action.friend]}
+        case 'REMOVE_FRIEND':
+            const friendIndex = (e) => e.id === action.id
+            const newList = {...state}
+            const i = newList.friends.findIndex(friendIndex)
+            const friends = newList.friends.slice(0, i).concat(newList.friends.slice(-i))
+            return {friends: friends}
+        default:
+            return state
+    }
 }
